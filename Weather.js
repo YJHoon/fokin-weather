82bff994ec4b1939e1f2ddd91de67e12475abef6
YJHoon <yjhoon0816@gmail.com>
2020-12-24 00:39:12 +0900

    날씨 앱 완성

--- Weather.js
+++ Weather.js
@@ -1,20 +1,90 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { LinearGradient } from 'expo-linear-gradient';
 
-export default function Weather({temp}) {
+
+const weatherOptions = {
+  Haze: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "날씨 앱 제목",
+    subtitle: "날씨 앱 부제목"
+  },
+  Thunderstorm: {
+    iconName: "weather-lightning-rainy",
+    gradient: ["#00C6FB", "#005BEA"],
+    title: "날씨 앱 제목",
+    subtitle: "날씨 앱 부제목"
+  },
+  Drizzle: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+    title: "날씨 앱 제목",
+    subtitle: "날씨 앱 부제목"
+  },
+  Rain: {
+    iconName: "weather-rainy",
+    gradient: ["#00C6FB", "#005BEA"],
+    title: "날씨 앱 제목",
+    subtitle: "날씨 앱 부제목"
+  },
+  Snow: {
+    iconName: "weather-snowy",
+    gradient: ["#7DE2FC", "#B9B6E5"],
+    title: "날씨 앱 제목",
+    subtitle: "날씨 앱 부제목"
+  },
+  Atmosphere: {
+    iconName: "weather-hail",
+    gradient: ["#89F7FE", "#66A6FF"],
+    title: "날씨 앱 제목",
+    subtitle: "날씨 앱 부제목"
+  },
+  Clear: {
+    iconName: "weather-sunny",
+    gradient: ["FF7300", "#FEF253"],
+    title: "날씨 앱 제목",
+    subtitle: "날씨 앱 부제목"
+  },
+  Clouds: {
+    iconName: "weather-cloudy",
+    gradient: ["#D7D2CC", "#304352"],
+    title: "날씨 앱 제목",
+    subtitle: "날씨 앱 부제목"
+  },
+  Mist: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "날씨 앱 제목",
+    subtitle: "날씨 앱 부제목"
+  },
+  Dust: {
+    iconName: "weather-hail",
+    gradient: ["#4DA0B0", "#D39D38"],
+    title: "날씨 앱 제목",
+    subtitle: "날씨 앱 부제목"
+  }
+}
+
+export default function Weather({temp, condition}) {
   return(
-    <View style={styles.container}>
+    <LinearGradient
+      // Background Linear Gradient
+      colors={weatherOptions[condition].gradient}
+      style={styles.container}
+    >
+      <StatusBar barStyle="light-content" />
       <View style={styles.halfContainer}>
-        <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
+        <MaterialCommunityIcons size={96} name={weatherOptions[condition].iconName} color="white" />
         <Text style={styles.temp}>{temp}</Text>
       </View>
-      <View style={styles.halfContainer}>
-        <MaterialCommunityIcons size={86} name="weather-lightning-rainy" />
-        <Text>{temp}</Text>
+      <View style={{...styles.halfContainer, ...styles.textContainer}}>
+        <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+        <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
       </View>
-    </View>
+    </LinearGradient>
   )
 }
 
@@ -35,6 +105,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   temp: {
-    fontSize: 42
+    fontSize: 42,
+    color: "white"
+  },
+  title: {
+    color: "white",
+    fontSize: 44,
+    fontWeight: "300",
+    marginBottom: 10
+  },
+  subtitle: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 24
+  },
+  textContainer: {
+    paddingHorizontal: 20,
+    alignItems: "flex-start"
   }
 })
