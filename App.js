abc001bc290aa9d4c1b5f5224503a13393048b59
YJHoon <yjhoon0816@gmail.com>
2020-12-24 00:08:10 +0900

    아이콘추가

--- App.js
+++ App.js
@@ -18,12 +18,13 @@ export default class extends React.Component {
   }
 
   getWeather = async(latitude, longitude) => {
-    const { data } = await axios.get(
+    const { data: {main: {temp}, weather} } = await axios.get(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
       );
       this.setState({ 
         isLoading: false, 
-        temp: data.main.temp
+        temp,
+        condition: weather[0].main
       });
   };
 
@@ -40,7 +41,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading, temp } = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}/>;
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition}/>;
   }
 }
--- Weather.js
+++ Weather.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import { View, Text, StyleSheet } from 'react-native';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Weather({temp}) {
   return(
     <View style={styles.container}>
-      <Text>{temp}</Text>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons size={96} name="weather-lightning-rainy" />
+        <Text style={styles.temp}>{temp}</Text>
+      </View>
+      <View style={styles.halfContainer}>
+        <MaterialCommunityIcons size={86} name="weather-lightning-rainy" />
+        <Text>{temp}</Text>
+      </View>
     </View>
   )
 }
 
 Weather.propTypes = {
-  temp: propTypes.number.isRequired
+  temp: propTypes.number.isRequired,
+  condition: propTypes.oneOf(["Thunderstorm", "Drizzle", "Rain", "Snow", "Atmosphere", "Clear", "Clouds", "Haze", "Mist", "Dust"]).isRequired
 }
 
 const styles = StyleSheet.create({
@@ -19,6 +28,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-
+  },
+  halfContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 42
   }
 })
